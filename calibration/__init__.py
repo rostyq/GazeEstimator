395c9f390e40be0e9fbce5a0a3379a2668f2d311
rostyslavb <rostyslav.db@gmail.com>
2018-04-11 19:01:46 +0300

    calibration with dataset + bug fixes

--- calibration/__init__.py
+++ calibration/__init__.py
@@ -1,9 +1,10 @@
 from time import strftime
-from os import path, getcwd, listdir
+from os import path, listdir, getcwd
 from yaml import dump, load
 from logging import basicConfig, getLogger, DEBUG
-from numpy import zeros, mgrid, float32, array, asarray
-from cv2 import *
+import numpy as np
+import cv2
+import json
 
 basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
             filename='log/{}.log'.format(__name__),
@@ -12,8 +13,8 @@ logger = getLogger(__name__)
 
 class Calibration:
 
-    def __init__(self):
-        self.path = path.join(getcwd(), 'calibration/dataset')
+    def __init__(self, path_to_dataset):
+        self.path = path_to_dataset if path is not None else path.join(getcwd(), 'calibration/dataset')
         self.metadata = path.join(self.path, '{}.yaml'.format(__name__))
         self.object_points = list()
         self.frame_points = list()
@@ -31,32 +32,32 @@ class Calibration:
             logger.error('Take more images')
 
     def capture_calibration(self):
-        capture = VideoCapture(CAP_ANY)
+        capture = cv2.VideoCapture(cv2.CAP_ANY)
         while capture.isOpened():
             self.retrieval, self.frame = capture.read()
             self.display_message('Saved frames: {}'.format(self.counter))
-            imshow(__name__, self.frame)
-            if waitKey(33) % 256 == 27:
+            cv2.imshow(__name__, self.frame)
+            if cv2.waitKey(33) % 256 == 27:
                 logger.warning('ESC pressed, closing...')
                 break
-            if waitKey(33) % 256 == 32:
+            if cv2.waitKey(33) % 256 == 32:
                 self.retrieval, corners = self.find_corners()
                 if self.retrieval:
                     self.counter += 1
-                    imwrite(self.frame_path(), self.frame)
+                    cv2.imwrite(self.frame_path(), self.frame)
                     logger.info('Frame was saved to {}'.format(self.frame_path()))
                     self.object_points.append(self.zero_points())
                     self.draw_corners(corners)
-                    imshow(__name__, self.frame)
+                    cv2.imshow(__name__, self.frame)
         capture.release()
-        cv.destroyAllWindows()
+        cv2.destroyAllWindows()
 
     def dataset_calibration(self, dataset_path=''):
         dataset_path = self.path if not dataset_path else dataset_path
-        for filename in os.listdir(dataset_path):
-            frame = os.path.join(dataset_path, filename)
-            if os.path.isfile(frame):
-                self.frame = cv.imread(frame)
+        for filename in listdir(dataset_path):
+            frame = path.join(dataset_path, filename)
+            if path.isfile(frame):
+                self.frame = cv2.imread(frame)
                 self.retrieval, corners = self.find_corners()
                 if self.retrieval:
                     self.counter += 1
@@ -65,7 +66,7 @@ class Calibration:
 
     def calibrate_camera(self):
         self.retrieval, self.matrix, self.distortion, self.rotation, \
-        self.translation = cv.calibrateCamera(self.object_points,
+        self.translation = cv2.calibrateCamera(self.object_points,
                                            self.frame_points,
                                            self.frame_to_grey().shape[::-1],
                                            None, None)
@@ -93,29 +94,29 @@ class Calibration:
         logger.info('Translation vectors: {}'.format(self.translation))
 
     def find_corners(self):
-        return cv.findChessboardCorners(self.frame_to_grey(), (6,9))
+        return cv2.findChessboardCorners(self.frame, (4,6))
 
     def corners_subpixel(self, corners):
-        criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-        return cv.cornerSubPix(self.frame_to_grey(), corners, (11,11), (-1,-1), criteria)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        return cv2.cornerSubPix(self.frame_to_grey(), corners, (11,11), (-1,-1), criteria)
 
     def draw_corners(self, corners):
         corners_subpixel = self.corners_subpixel(corners)
         self.frame_points.append(corners_subpixel)
-        cv.drawChessboardCorners(self.frame, (6,9), corners_subpixel, self.retrieval)
+        cv2.drawChessboardCorners(self.frame, (5,7), corners_subpixel, self.retrieval)
 
     def display_message(self, message):
-        cv.putText(self.frame, message, (20, 40), 0, 0.6, (0, 0, 0), 2)
+        cv2.putText(self.frame, message, (20, 40), 0, 0.6, (0, 0, 0), 2)
 
     def zero_points(self):
-        points = np.zeros((6*9,3), np.float32)
-        points[:,:2] = np.mgrid[0:6,0:9].T.reshape(-1,2)
+        points = np.zeros((4*6,3), np.float32)
+        points[:,:2] = np.mgrid[0:4,0:6].T.reshape(-1,2)
         return points
 
     def frame_path(self):
-        return os.path.join(self.path, time.strftime('%Y%m%d%H%M%S.png'))
+        return path.join(self.path, strftime('%Y%m%d%H%M%S.png'))
 
     def frame_to_grey(self):
-        return cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
+        return cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
 
 ################################################################################
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+#Hello
 from calibration import Calibration
 
 if __name__ == '__main__':
-    camera = Calibration()
-    camera.calibrate()
+    camera = Calibration(path_to_dataset=r'C:\Users\Beehiveor\Documents\BAS\GazeEstimator\calibration\stand_dataset')
+    camera.calibrate(method='dataset')
