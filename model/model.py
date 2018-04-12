bddeb0748c4fad29fe95f82237fa2706a5a083e5
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2018-04-12 17:04:59 +0300

    keras model
    average acc 7 degree

--- model/model.py
+++ model/model.py
@@ -1,14 +1,20 @@
 from keras.models import load_model
-from model.utils import gaze2Dto3D, pose3Dto2D, angle_loss, angle_accuracy
+from transform import gaze2Dto3D, pose3Dto2D
+from nn import angle_accuracy
 
-scope_dict = {'angle_loss': angle_loss, 'angle_accuracy': angle_accuracy}
+scope_dict = {'angle_accuracy': angle_accuracy}
 
-model = load_model('./model_epoch1.h5', custom_objects=scope_dict, compile=True)
-
+model = load_model('./model_master.h5', custom_objects=scope_dict, compile=True)
 
 def estimate_gaze(eye_image, head_pose):
-    eye_image = eye_image.reshape((-1, 36, 60, 1))
+
+    # prepare and transform
+    eye_image = eye_image.reshape((-1, 36, 60, 1)) / 255
     head_pose = head_pose.reshape((-1, 3))
     head_pose = pose3Dto2D(head_pose)
+
+    # predict
     gaze = model.predict([eye_image, head_pose])
+
+    # transform and return
     return gaze2Dto3D(gaze)
