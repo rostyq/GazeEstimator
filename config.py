37cfad843af0f1eaf677e77fcfcdd6fdd699dbb0
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2018-05-02 17:26:01 +0300

    Refactoring
    - refactored `run_experiment` function to `run_coarse_experiment`, added
    docstring
    - removed Ticker and Capture classes
    - replace `dummy_head_pose` declaration to `run_coarse_experiment`
    - refactored estimator module, added init_model function, decomposed
    estimation process
    - made `main.py` config oriented
    - wrote some default variables to `config.py`

---
+++ config.py
@@ -0,0 +1,11 @@
+# path to gaze estimator model
+PATH_TO_ESTIMATOR = './app/binaries/estimator.h5'
+
+# default config for coarse experiment
+ESCAPE = 27
+DEFAULT_WEBCAM_CAPTURE_TARGET = 0
+TEST_TICKS_TRESHOLD = 10
+DEFAULT_AVERAGE_DISTANCE = 12
+
+# screen parameters
+screen_diagonal = 13.3
