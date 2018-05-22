a194acb3122f630271fda94ce74748a7e84f8505
Mykhailo Chaus <mchaus97@gmail.com>
2018-05-22 14:47:09 +0300

    Add function for converting coordinates to screen pixels

--- app/device/screen.py
+++ app/device/screen.py
@@ -65,6 +65,14 @@ class Screen(SceneObj):
         self.resolution = resolution
         self.height = height
         self.width = width
+        if self.height is not None:
+            self.pixel_height = self.height / self.resolution[0]
+        else:
+            self.pixel_height = None
+        if self.width is not None:
+            self.pixel_width = self.width / self.resolution[1]
+        else:
+            self.pixel_width = None
         self.mpp = None
 
     def calc_mpp(self):
@@ -83,8 +91,26 @@ class Screen(SceneObj):
         point = array([coord*axis*self.mpp for coord, axis in zip((x, y), self.resolution)]+[0.0])
         return self.get_rotation_matrix() @ point - self.translation
 
-
+    def point_in_pixels(self, x, y):
+        if self.width is not None and self.height is not None:
+            pixel_x, pixel_y = round(x / self.pixel_height), round(y/ self.pixel_width)
+        else:
+            pixel_x, pixel_y = None, None
+        return pixel_x, pixel_y
 
 if __name__ == '__main__':
+    print('Plane intersection example:\n'
+          'Plane = ((2,0,0), (0,-4,0), (0,0,4))\n'
+          'Line = ((1, 3, -1), (3,4,2))\n'
+          'Right answer: [3, 4, 2]\n')
     print(plane_line_intersection(((1, 3, -1), (3,4,2)), ((2,0,0), (0,-4,0), (0,0,4))))
-    print('Right answer: [3, 4, 2]')
+
+    print('Point in pixels:\n'
+          'height = 100 mm\n'
+          'width = 200 mm\n'
+          'resolution = (1000, 2000)\n'
+          'point = (10.123, 20,123)\n')
+
+    new_screen = Screen('Dell', origin=None, matrix=None, distortion=None,
+                 diagonal=sqrt(5) * 100, resolution=(1000, 2000), height=100, width=200)
+    print(new_screen.point_in_pixels(*(10.123, 20.123)))
